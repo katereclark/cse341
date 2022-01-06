ff84d09873c320ce997172b5b70d3c1dfc900df3
katereclark <cla16022@byui.edu>
2022-01-06 10:59:07 -0700

    Added assignment 1

---
+++ app-assignment.js
@@ -0,0 +1,38 @@
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+    const url = req.url;
+
+    if (url === '/') {
+        res.write('<html>');
+        res.write('<head><title>Assignment 1</title></head>');
+        res.write('<body><form action="/create-user" method="POST"><input tpye="text" name="username"><button type="submit">Create User</button></form></body>');
+        res.write('</html>');
+        return res.end();
+    }
+
+    if (url === '/users') {
+        res.setHeader('Content-Type', 'text/html');
+        res.write('<html>');
+        res.write('<head><title>Assignment 1</title></head>');
+        res.write('<body><ul><li>User 1</li><li>User 2</li><li>User 3</li></ul></body>');
+        res.write('</html>');
+        return res.end();
+    }
+
+    if (url === '/create-user') {
+        const body = [];
+        req.on('data', (chunk) => {
+            body.push(chunk);
+        });
+        req.on('end', () => {
+            const parsedBody = Buffer.concat(body).toString();
+            console.log(parsedBody.split('=')[1]);
+        });
+        res.statusCode = 302;
+        res.setHeader('Location', '/');
+        res.end();
+    }
+});
+
+server.listen(3000);
