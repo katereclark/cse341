e1e67be8647bacc0a3611ea9637a7c926deb768c
katereclark <cla16022@byui.edu>
2022-01-11 16:57:16 -0700

    Fixed errors

--- routes/admin.js
+++ routes/admin.js
@@ -12,7 +12,7 @@ router.get('/add-product', (req, res, next) => {
 });
 
 router.post('/add-product', (req, res, next) => {
-    products.push({title: req.body.title});
+    products.push({title: req.body.title, price: req.body.price, description: req.body.description});
     res.redirect('/');
 });
 
