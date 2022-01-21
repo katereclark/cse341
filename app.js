bb46cb945f80d7690a4f5b9a932b010a626ded83
katereclark <cla16022@byui.edu>
2022-01-21 12:17:45 -0700

    Added cart functionality and deleting ability

--- app.js
+++ app.js
@@ -1,18 +1,19 @@
-const express = require('express');
-const bodyParser = require('body-parser');
 const path = require('path');
 
-const app = express();
-
-const adminRoutes = require('./routes/admin');
-const shopRoutes = require('./routes/shop');
+const express = require('express');
+const bodyParser = require('body-parser');
 
 const errorController = require('./controllers/error');
+
+const app = express();
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
-app.use(bodyParser.urlencoded({extended: false}));
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/admin', adminRoutes);
